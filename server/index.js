8dbf9f5c6f343c3552cad68d11cf06f4206704ae
Rahul <rahuls@live.ca>
2021-07-12 13:18:07 +0530

    submitting partial

---
+++ server/index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+const cors = require('cors');
+
+
+app.use(cors({origin: "*"}))
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+        socket.broadcast.emit('chat message', msg);
+    });
+});
+http.listen(3000, () => {
+    console.log('listening on *:3000');
+});
